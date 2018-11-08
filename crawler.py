81f98a21db28feb76c947cff2bd8557c2ee7c412
José Victor Viriato <jvviriato@inf.ufsm.br>
2018-11-08 13:49:10 -0200

    +nomes

--- crawler.py
+++ crawler.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup as bs
 cat_names = []
 soup = bs(open("gatos.html"), "html.parser")
 for tr in soup.find_all('tr'):
-    tds = tr.find_all('td')[0].text
-    if "Nomes" not in tds:
-        cat_names.append(tds)
+    for l in range(0,len(tr.find_all('td'))):
+        tds = tr.find_all('td')[l].text
+        if "Nomes" not in tds and tds != "":
+            cat_names.append(tds)
 
 file = open('cat_names.txt', 'w+')
 for cat_name in cat_names:
