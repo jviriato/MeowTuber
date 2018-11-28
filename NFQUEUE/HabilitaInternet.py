7aa410287c6e299dabe9fcdbfd87254871460817
MzGrisotti <mzgrisotti@inf.ufsm.br>
2018-11-28 05:27:02 -0200

    Update HabilitaInternet.py

--- NFQUEUE/HabilitaInternet.py
+++ NFQUEUE/HabilitaInternet.py
@@ -1,19 +1,15 @@
-
 from scapy.all import *
 from netfilterqueue import NetfilterQueue
 
-def main():
-    q = NetfilterQueue()
-    q.bind(0, socket.AF_INET)
-    q.set_callback(process)
-    q.create_queue(0)
- 
-    try:
-       q.try_run()
-    except KeyboardInterrupt:
-       print "Exiting..."
-    q.unbind(socket.AF_INET)
-    q.close()
-    sys.exit(1)
- 
-main()
+def print_and_accept(pkt):
+    data = pkt.get_payload()
+    p = IP(data)
+    #print p
+    pkt.accept()
+
+nfqueue = NetfilterQueue()
+nfqueue.bind(0, print_and_accept)
+try:
+    nfqueue.run()
+except KeyboardInterrupt:
+    print
