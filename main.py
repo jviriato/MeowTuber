17e11732d188fe646cb0657b4e4bf57ae0809182
José Victor Viriato <jvviriato@inf.ufsm.br>
2018-11-28 01:20:34 -0200

    painel adm

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from flask import Flask, url_for, render_template, request, redirect, url_for
 
+bad_words = []
+bd_count = 0
+class Badword:
+    def __init__(self, nome, id):
+        self.id = id
+        self.nome = nome
+
+def load_badwords():
+    global bd_count
+    with open("badwords.txt", "r") as bd:
+        for line in bd:
+            line.replace('\n', '')
+            print(line)
+            bd_count += 1
+            b = Badword(line, bd_count)
+            bad_words.append(b)
+            print(b)
+
+load_badwords()
+
+
 app = Flask(__name__)
-# url_for('static', filename='main.css')
 
 @app.route('/', methods=['POST', 'GET'])
 def index(bad_words = None):
@@ -16,8 +36,9 @@ def submit():
     return 'You entered: {}'.format(text)
 
 @app.route('/admin')
-def admin():
-    return 'admin'
+def admin(bad_words = bad_words):
+    print(bad_words)
+    return render_template('admin.html', bad_words=bad_words)
 
 @app.route('/text')
 def text():
